761e81eff2e40e5413115eaa8f9f927a82cca47d
St <valuable_information@mail.ru>
2025-04-14 15:56:37 +0300

    add function

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-def open_file(file: str) -> dict:
+from pprint import pprint
+
+def open_file(file: str) -> dict[str, list[dict[str, int | str]]]:
     """
     Открывает указанный .txt-файл и возвращает содержимое в виде словаря.
+
     :param file: Путь к файлу с расширением '.txt'.
     :type file_path: str
     :return: Словарь, сформированный на основе содержимого файла. 
-    
     """
-     
     cook_book = {}
 
     with open(file, 'r', encoding='utf-8') as f:
@@ -30,7 +31,7 @@ def open_file(file: str) -> dict:
 
     return cook_book   
 
-def pretty_print(cook_book: dict) -> None:
+def pretty_print(cook_book: dict[str, list[dict[str, int | str]]]) -> None:
     """
     Функция для красивого форматированного вывода книги рецептов.
 
@@ -47,7 +48,40 @@ def pretty_print(cook_book: dict) -> None:
     print("}")
 
 
+def get_shop_list_by_dishes(dishes: list[str], person_count: int) -> tuple[dict[str, dict[str, str | int]], list[str]]: 
+    """
+    Функция рассчитывает количество ингредиентов для приготовления блюд на указанное количество гостей.
+
+    :param dishes: Список, содержащий названия блюд.
+    :type dishes: List[str]
+    :return: Кортеж, содержащий словарь с ингредиентами и список отсутствующих блюд.
+    """
+    cook_lst = {}
+    missing_dishes = []
+
+    for dish in dishes:
+        if dish in cook_book:
+            for ingredient in cook_book[dish]:
+                ingr = ingredient['ingredient_name']
+                quan = ingredient['quantity'] * person_count
+                mea = ingredient['measure']
+
+                if ingr in cook_lst:
+                    cook_lst[ingr]['quantity'] += quan
+                else:
+                    cook_lst[ingr] = {'measure': mea, 'quantity': quan}
+        else:
+            missing_dishes.append(dish)
+
+    return cook_lst, missing_dishes
 
 cook_book = open_file('recipes.txt')
-pretty_print(cook_book)
+# pretty_print(cook_book)
+
+res, missing = get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+pprint(res)
+
+if missing: # поздно увидела, что в задании прописано, что список блюд из cook_book и других быть не может)) но оставимс
+    print("\nОтсутствующие блюда:")
+    print(", ".join(missing))
 
